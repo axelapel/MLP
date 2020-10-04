06dae36192a17b4189924fe68a2c5541fae293d1
Axel Lapel <axel.lapel@gmail.com>
2020-10-04 16:12:59 +0200

    restructuration

--- FCnet.py
+++ FCnet.py
@@ -88,8 +88,7 @@ class MLP:
             self.hidden.append(dot)
             input_vector = self.sigmoid(dot)
             self.activations.append(input_vector)
-        self.output = input_vector
-        return self.output
+        return input_vector
 
     def backward(self, estimation, training_y):
         """Backward propagation to correct the weights/biases.
@@ -103,16 +102,14 @@ class MLP:
         """
         error = (estimation - training_y).reshape(-1, 1)
         for i in reversed(range(self.number_layers + 1)):
-            pdb.set_trace()
-            delta = np.multiply(error,
-                                self.sig_der(self.hidden[i].reshape(-1, 1)))
+            # pdb.set_trace()
+            # delta = np.multiply(error,
+            #                    self.sig_der(self.hidden[i].reshape(-1, 1)))
+            delta = error * self.sig_der(self.hidden[i])
             self.bias_errors.append(delta[:, 0])
-            grad = np.dot(self.activations[i].reshape(-1, 1), delta.T)
+            grad = np.dot(self.activations[i].reshape(-1, 1), delta)
             self.gradients.append(grad)
-            error = np.dot(self.weights[i], delta)
-
-        # The dimensions are good but the values of gradients/delta are
-        # exagerated : see values.
+            error = np.dot(delta, self.weights[i].T)
 
     def grad_descent(self, learning_rate):
         """Gradient descent to correct the weights by minimizing the
@@ -126,12 +123,8 @@ class MLP:
         self.gradients.reverse()
         self.bias_errors.reverse()
         for i in range(self.number_layers + 1):
-            # pdb.set_trace()
             self.weights[i] -= learning_rate * self.gradients[i]
             self.biases[i] -= learning_rate * self.bias_errors[i]
-
-        # WEIGHTS and BIASES ARE WAY TOO LARGE !
-        # ---> Problem in the backpropagation
 
     def get_weights(self):
         """ Getter method to keep track of weights arrays."""
@@ -141,31 +134,48 @@ class MLP:
 if __name__ == "__main__":
 
     # Hyper-parameters
-    n_in = 3
+    n_sample = 10
+    n_in = 2
     n_out = 1
     n_lay = 4
     sz_hid = 10
     lr = 1e-3
 
-    # Training I/O
-    train_x = np.random.rand(n_in)
-    train_y = np.random.rand(n_out)
+    ################ Training I/O ################
+
+    # XOR test
+    # train_x = np.random.randint(0, 2, (n_sample, 2))
+    # train_y = np.random.randint(0, 2, n_sample)
+
+    # Summation
+    train_x = np.random.uniform(0, 0.5, n_in)
+    train_y = np.sum(train_x)
+
+    # Random
+    # train_x = np.random.rand(n_in)
+    # train_y = np.random.rand(n_out)
 
     # NN instance
-    MLP = MLP(n_lay, sz_hid, n_in, n_out)
+    net = MLP(n_lay, sz_hid, n_in, n_out)
 
-    #y_hat = MLP.forward(x)
-    #print("Output : {}".format(y_hat))
-    # W = MLP.get_weights()
-    # print(W[0])
-
+    # Training
     epochs = np.arange(100)
-    loss = np.zeros_like(epochs)
+    loss = []
     for epoch in epochs:
-        output = MLP.forward(train_x)
-        MLP.backward(output, train_y)
-        MLP.grad_descent(lr)
-        loss[epoch] = MLP.loss(output, train_y)
+        output = net.forward(train_x)
+        net.backward(output, train_y)
+        net.grad_descent(lr)
+        loss_ = net.loss(output, train_y)
+        loss.append(loss_)
 
     fig, ax = plt.subplots()
     ax.plot(epochs, loss)
+
+    # Test I/O
+    test_x = np.random.uniform(0, 0.5, n_in)
+    test_y = np.sum(test_x)
+
+    # Forward pass
+    estimation = net.forward(test_x)
+    print("True output : {:.2f}\nEstimation : {:.2f}".format(
+        test_y, estimation[0]))
